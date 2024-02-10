f6f84216cb7008ae12fc5c276dbfbd26e132a428
Waqar Ahmed <waqar.17a@gmail.com>
2024-02-10 13:53:44 +0500

    Improved scroll to surah/para in drawer

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -22,8 +22,6 @@ class _MainPageState extends State<MainPage>
     with WidgetsBindingObserver, TickerProviderStateMixin {
   late final ParaAyatModel _paraModel;
   final ScrollController _scrollController = ScrollController();
-  final ScrollController _paraListScrollController = ScrollController();
-  final ScrollController _surahListScrollController = ScrollController();
   late final TabController _drawerTabController;
   PageController _pageController = PageController(keepPage: false);
 
@@ -31,7 +29,6 @@ class _MainPageState extends State<MainPage>
   void initState() {
     _paraModel = ParaAyatModel(onParaChanged);
     _drawerTabController = TabController(length: 2, vsync: this);
-    _drawerTabController.addListener(_onDrawerTabChange);
 
     WidgetsBinding.instance.addObserver(this);
     super.initState();
@@ -39,7 +36,6 @@ class _MainPageState extends State<MainPage>
 
   @override
   void dispose() {
-    _drawerTabController.removeListener(_onDrawerTabChange);
     _paraModel.dispose();
     WidgetsBinding.instance.removeObserver(this);
     super.dispose();
@@ -58,41 +54,6 @@ class _MainPageState extends State<MainPage>
     // reinstall listeners
     _pageController.removeListener(_saveScrollPosition);
     _pageController.addListener(_saveScrollPosition);
-  }
-
-  void _onDrawerTabChange() {
-    if (!_drawerTabController.indexIsChanging) {
-      //listen to false so we handle both drag and tap
-      if (_drawerTabController.index == 0) {
-        _scrollToParaInDrawer();
-      } else if (_drawerTabController.index == 1) {
-        _scrollToPosition(_surahListScrollController, () {
-          int surah = firstSurahInPara(_paraModel.currentPara - 1);
-          return 48 * surah.toDouble();
-        });
-      }
-    }
-  }
-
-  void _scrollToParaInDrawer() {
-    _scrollToPosition(_paraListScrollController, () {
-      int currentParaIdx = _paraModel.currentPara - 1;
-      return currentParaIdx > 8 ? 48 * (currentParaIdx - 3) : 0;
-    });
-  }
-
-  static void _scrollToPosition(
-      ScrollController controller, double Function() getJumpPos) {
-    if (controller.hasClients) {
-      double jump = getJumpPos();
-      if (jump > controller.position.maxScrollExtent) {
-        jump = controller.position.maxScrollExtent;
-      }
-      controller.jumpTo(jump);
-    } else {
-      Future.delayed(const Duration(milliseconds: 10),
-          () => _scrollToPosition(controller, getJumpPos));
-    }
   }
 
   Future<void> _load() async {
@@ -297,75 +258,98 @@ class _MainPageState extends State<MainPage>
           );
         },
       ),
-      drawer: Drawer(
-        child: SafeArea(
-          child: DefaultTabController(
-            animationDuration: const Duration(milliseconds: 150),
-            length: 2,
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                SizedBox(
-                  height: 50,
-                  child: TabBar(
-                    controller: _drawerTabController,
-                    tabs: const [
-                      Tab(text: "Para"),
-                      Tab(text: "Surah"),
-                    ],
+      drawer: Builder(builder: (context) {
+        int maxVisibleItems =
+            ((MediaQuery.of(context).size.height - (48 + 4)) / 48).floor();
+        const totalParas = 30;
+        final maxScrollablePara = totalParas -
+            maxVisibleItems; // if we scroll to the bottom, this para is visible at top
+
+        int currentParaIdx = _paraModel.currentPara - 1;
+        int paraScrollTo = 0;
+        if (currentParaIdx > 8 && currentParaIdx < 20) {
+          paraScrollTo = 48 * (currentParaIdx - 3);
+        } else if (currentParaIdx > maxScrollablePara) {
+          paraScrollTo = 48 * maxScrollablePara;
+        }
+
+        final paraListScrollController = ScrollController(
+            initialScrollOffset: paraScrollTo.toDouble(),
+            keepScrollOffset: false);
+
+        int surah = firstSurahInPara(_paraModel.currentPara - 1);
+        double surahScrollTo = 48 * surah.toDouble();
+        final surahListScrollController = ScrollController(
+            initialScrollOffset: surahScrollTo, keepScrollOffset: false);
+
+        return Drawer(
+          child: SafeArea(
+            child: DefaultTabController(
+              animationDuration: const Duration(milliseconds: 150),
+              length: 2,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  SizedBox(
+                    height: 50,
+                    child: TabBar(
+                      controller: _drawerTabController,
+                      tabs: const [
+                        Tab(text: "Para"),
+                        Tab(text: "Surah"),
+                      ],
+                    ),
                   ),
-                ),
-                Expanded(
-                  child: TabBarView(
-                    controller: _drawerTabController,
-                    children: [
-                      ListView.builder(
-                        controller: _paraListScrollController,
-                        scrollDirection: Axis.vertical,
-                        itemCount: 30,
-                        itemExtent: 48,
-                        itemBuilder: (context, index) {
-                          return paraListItem(index);
-                        },
-                      ),
-                      ListView.builder(
-                        controller: _surahListScrollController,
-                        scrollDirection: Axis.vertical,
-                        itemCount: 114,
-                        itemExtent: 48,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: Text(
-                              "${index + 1}.",
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(fontSize: 20),
-                            ),
-                            title: Text(
-                              surahDataForIdx(index, arabic: true).name,
-                              style: const TextStyle(
-                                letterSpacing: 0,
-                                fontSize: 24,
-                                fontFamily: 'Al Mushaf',
+                  Expanded(
+                    child: TabBarView(
+                      controller: _drawerTabController,
+                      children: [
+                        ListView.builder(
+                          controller: paraListScrollController,
+                          scrollDirection: Axis.vertical,
+                          itemCount: 30,
+                          itemExtent: 48,
+                          itemBuilder: (context, index) {
+                            return paraListItem(index);
+                          },
+                        ),
+                        ListView.builder(
+                          controller: surahListScrollController,
+                          scrollDirection: Axis.vertical,
+                          itemCount: 114,
+                          itemExtent: 48,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: Text(
+                                "${index + 1}.",
+                                textAlign: TextAlign.center,
+                                style: const TextStyle(fontSize: 20),
                               ),
-                            ),
-                            onTap: () => _onSurahTapped(index),
-                          );
-                        },
-                      ),
-                    ],
+                              title: Text(
+                                surahDataForIdx(index, arabic: true).name,
+                                style: const TextStyle(
+                                  letterSpacing: 0,
+                                  fontSize: 24,
+                                  fontFamily: 'Al Mushaf',
+                                ),
+                              ),
+                              onTap: () => _onSurahTapped(index),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
-        ),
-      ),
+        );
+      }),
       onDrawerChanged: (opened) {
         if (!opened) return;
         if (_drawerTabController.index != 0) {
           _drawerTabController.animateTo(0);
-        } else {
-          _scrollToParaInDrawer();
         }
       },
     );
