d8f62787175df76c5d3c57151ac218ff37aac2f1
Waqar Ahmed <waqar.17a@gmail.com>
2025-02-08 18:55:51 +0500

    Use named record fields
    Hard to read $1, $2

--- lib/models/ayah_selection_model.dart
+++ lib/models/ayah_selection_model.dart
@@ -1,49 +1,55 @@
 import 'package:quran_memorization_helper/models/ayat.dart';
 
 class AyahSelectionState {
-  List<(int ayahIdx, bool selected)> _selection = [];
+  List<({int ayahIdx, bool selected})> _selection = [];
 
   AyahSelectionState.fromAyahs(
       final List<AyatOrMutashabiha> ayahAndMutashabihas) {
     _selection = List.generate(ayahAndMutashabihas.length, (index) {
       final a = ayahAndMutashabihas[index];
       return a.ayat != null
-          ? (a.ayat!.ayahIdx, false)
-          : (a.mutashabiha!.src.ayahIdx, false);
+          ? (ayahIdx: a.ayat!.ayahIdx, selected: false)
+          : (ayahIdx: a.mutashabiha!.src.ayahIdx, selected: false);
     }, growable: false);
   }
 
   void toggle(int ayahIndex) {
-    int found = _selection.indexWhere((e) => e.$1 == ayahIndex);
+    int found = _selection.indexWhere((e) => e.ayahIdx == ayahIndex);
     while (found != -1) {
-      _selection[found] = (_selection[found].$1, !_selection[found].$2);
-      found = _selection.indexWhere((e) => e.$1 == ayahIndex, found + 1);
+      _selection[found] = (
+        ayahIdx: _selection[found].ayahIdx,
+        selected: !_selection[found].selected
+      );
+      found = _selection.indexWhere((e) => e.ayahIdx == ayahIndex, found + 1);
     }
   }
 
   void selectAll() {
     if (_selection.isEmpty) return;
-    final firstSelected = _selection[0].$2;
+    final firstSelected = _selection[0].selected;
 
     for (int i = 0; i < _selection.length; ++i) {
-      _selection[i] = (_selection[i].$1, !firstSelected);
+      _selection[i] =
+          (ayahIdx: _selection[i].ayahIdx, selected: !firstSelected);
     }
   }
 
   void clearSelection() {
     for (int i = 0; i < _selection.length; ++i) {
-      _selection[i] = (_selection[i].$1, false);
+      _selection[i] = (ayahIdx: _selection[i].ayahIdx, selected: false);
     }
   }
 
   bool isSelected(int index) {
-    return index < _selection.length ? _selection[index].$2 : false;
+    return index < _selection.length ? _selection[index].selected : false;
   }
 
   List<int> selectedAyahs() {
     List<int> toRemove = [];
     for (final e in _selection) {
-      if (!toRemove.contains(e.$1)) toRemove.add(e.$1);
+      if (!toRemove.contains(e.ayahIdx)) {
+        toRemove.add(e.ayahIdx);
+      }
     }
     return toRemove;
   }
