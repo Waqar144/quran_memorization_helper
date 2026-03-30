25c5f3fe068d1ed61ca403fdea0a9681129d4077
Waqar Ahmed <waqar.17a@gmail.com>
2026-03-30 12:11:22 +0500

    Remove comment

--- test/para_model_test.dart
+++ test/para_model_test.dart
@@ -1,12 +1,4 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 // ignore_for_file: avoid_print
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:path_provider/path_provider.dart';
